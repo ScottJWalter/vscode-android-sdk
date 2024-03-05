8916bafba16e339464b56135e233f4eb1f571e93
Scott Walter <sjwalter@gmail.com>
2024-03-05 16:16:37 +0000

    fix sdk path

--- .devcontainer/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 'export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin' >> ~/.bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/platform-tools' >> ~/.bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/tools' >> ~/.bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/tools/bin' >> ~/.bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/emulator' >> ~/.bashrc
